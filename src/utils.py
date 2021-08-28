faf9ade1c8e2520197547ea5a7bb8b57ee7896cb
Daniel Bailo <danbailoufms@gmail.com>
2021-08-28 18:43:04 -0300

    implementada funcao para salvar os dados num arquivo json

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def print_data(data):
     print(f"Storage\t\tCPU\t\tMemory\t\tBandwidth\tPrice/mo\n")
@@ -8,11 +9,16 @@ def print_data(data):
 
 def save_csv(data):
     header = ["Storage", "CPU", "Memory", "Bandwidth", "Price/mo"]
-    with open("output/data.csv", "w") as file:
+    with open("output/data.csv", "w") as file:        
         writer = csv.writer(file)
         writer.writerow(header)
-        for row in data:
-            writer.writerow(row)
+        writer.writerows(data)
 
-def save_json():
-    pass
+def save_json(data):
+    header = ["Storage", "CPU", "Memory", "Bandwidth", "Price/mo"]
+    json_data = []
+    with open("output/data.json", "w", encoding="utf-8") as file:
+        for row in data:
+            storage, cpu, memory, bandwidth, price = row
+            json_data.append({"Storage":storage, "CPU":cpu, "Memory":memory, "Bandwidth":bandwidth, "Price/mo":price})
+        json.dump(json_data, file, ensure_ascii=False, indent=4,)
