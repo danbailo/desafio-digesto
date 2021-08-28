749ae49c6898aab8f3d4e70fec2832df193fc475
Daniel Bailo <danbailoufms@gmail.com>
2021-08-28 17:59:08 -0300

    dados organizados e armazenados numa lista

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-
 
 if __name__ == "__main__":
     target = "https://www.vultr.com/products/cloud-compute/#pricing"
     response = requests.get(target)
     soup = BeautifulSoup(response.text, "html.parser")
+    data = []
 
     table = soup.find("div", attrs=({"data-animation-options": "type:pricingTable;customInit: true;"}))
     rows = table.find_all("div", attrs=({"class":"pt__row-content"}))
     for cel in rows:
-        print(cel.find_all("strong"))
+        storage, cpu, memory, bandwidth, price = cel.find_all("strong")
+        data.append((storage.text, cpu.text, memory.text, bandwidth.text, price.text))
+
+    print(data)
