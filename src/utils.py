268551b064d9ee68925d32472d2e037cb12ae49d
Daniel Bailo <danbailoufms@gmail.com>
2021-08-28 18:09:36 -0300

    implementada funcao de print, simulando uma tabela

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
-def print_data():
-    pass
+def print_data(data):
+    print(f"Storage\t\tCPU\t\tMemory\t\tBandwidth\tPrice/mo\n")
+    for row in data:
+        storage, cpu, memory, bandwidth, price = row
+        print(f"{storage}\t\t{cpu}\t\t{memory}\t\t{bandwidth}\t\t{price}")
 
 def save_csv():
     pass
