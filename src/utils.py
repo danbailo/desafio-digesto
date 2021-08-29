708ad90c83a68128628d84acd13c06aef4052deb
Daniel Bailo <danbailoufms@gmail.com>
2021-08-29 14:56:51 -0300

    a funcao de print foi alterada para dividir os dados dos dois crawlers

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,13 @@ import json
 
 def print_data(data):
     print(f"CPU / VCPU\t\tMEMORY\t\tSTORAGE / SSD DISK\t\tBANDWIDTH / TRANSFER\t\tPRICE [ $/mo ]\n")
-    for row in data:
-        storage, cpu, memory, bandwidth, price = row.values()
-        print(f"{storage}\t\t\t{cpu}\t\t\t{memory}\t\t\t{bandwidth}\t\t\t{price}")
-
+    for k,v in data.items():
+        print(k)
+        for row in v:            
+            storage, cpu, memory, bandwidth, price = row.values()
+            print(f"{storage}\t\t\t{cpu}\t\t\t{memory}\t\t\t{bandwidth}\t\t\t{price}")
+        print()
+        
 def save_csv(data):
     with open("output/data.csv", "w") as file:        
         writer = csv.writer(file)
