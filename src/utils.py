d645a65d6d77ebfd0eb40c2869ceb330f31057ee
Daniel Bailo <danbailoufms@gmail.com>
2021-08-29 15:14:41 -0300

    implementado a funcao de salvar em csv com a divisao dos dados entre vultr e digitalocean

--- src/utils.py
+++ src/utils.py
@@ -2,20 +2,22 @@ import csv
 import json
 
 def print_data(data):
-    print(f"CPU / VCPU\t\tMEMORY\t\tSTORAGE / SSD DISK\t\tBANDWIDTH / TRANSFER\t\tPRICE [ $/mo ]\n")
+    print("CPU / VCPU\t\tMEMORY\t\tSTORAGE / SSD DISK\t\tBANDWIDTH / TRANSFER\t\tPRICE [ $/mo ]\n")
     for k,v in data.items():
         print(k)
         for row in v:            
             storage, cpu, memory, bandwidth, price = row.values()
             print(f"{storage}\t\t\t{cpu}\t\t\t{memory}\t\t\t{bandwidth}\t\t\t{price}")
         print()
-        
+
 def save_csv(data):
     with open("output/data.csv", "w") as file:        
         writer = csv.writer(file)
-        writer.writerow(data[0].keys())
-        for row in data:
-            writer.writerow(row.values())
+        writer.writerow("CPU / VCPU,MEMORY,STORAGE / SSD DISK,BANDWIDTH / TRANSFER,PRICE [ $/mo ]".split(","))
+        for k,v in data.items():
+            writer.writerow([k])
+            for row in v:            
+                writer.writerow(row.values())
 
 def save_json(data):
     with open("output/data.json", "w", encoding="utf-8") as file:
