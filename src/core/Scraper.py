d23964ee4c5097d3c82b67cb44ddb4eb59ae5e2c
Daniel Bailo <danbailoufms@gmail.com>
2021-08-29 02:47:35 -0300

    Dados da digitalocean coletados com sucesso! Os mesmos estao sendo armazenados numa lista de dicionarios. Tambem foi realizada a padronizacao de armazenamento de chaves entre os dois sites alvos

--- src/core/Scraper.py
+++ src/core/Scraper.py
@@ -1,7 +1,10 @@
+from typing import Optional
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import time
+
 
 class Scraper:
     def __init__(self):
@@ -11,23 +14,43 @@ class Scraper:
         target = "https://www.vultr.com/products/cloud-compute/#pricing"
         response = requests.get(target)
         soup = BeautifulSoup(response.text, "html.parser")
-
         table = soup.find("div", attrs=({"data-animation-options": "type:pricingTable;customInit: true;"}))
         rows = table.find_all("div", attrs=({"class":"pt__row-content"}))
         for cel in rows:
-            storage, cpu, memory, bandwidth, price = cel.find_all("strong")
-            self.data.append({"Storage":storage.text, "CPU":cpu.text, "Memory":memory.text, "Bandwidth":bandwidth.text, "Price/mo":price.text})
+            storage, cpu, memory, bandwidth, price_month = cel.find_all("strong")
+            self.data.append({
+                "CPU / VCPU": cpu.text, 
+                "MEMORY": memory.text, 
+                "STORAGE/SSD DISK": storage.text, 
+                "BANDWIDTH / TRANSFER": bandwidth.text, 
+                "PRICE [ $/mo ]": price_month.text})            
 
     def digitalocean(self):
         #https://www.digitalocean.com/page-data/pricing/page-data.json
 
         target = "https://www.digitalocean.com/pricing#droplet"
 
-        driver = webdriver.Firefox(executable_path="./geckodriver")
+        options = Options()
+        options.headless=True
+        driver = webdriver.Firefox(executable_path="./geckodriver", options=options)
         driver.get(target)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1)
         driver.find_element_by_xpath('//*[@id="heading"]/div[2]/div[1]/a[1]/div').click()
-        print(driver.page_source)
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="heading"]/div[2]/div/div[1]/div[2]/div[2]').click()
+        target_source = driver.page_source
         driver.close()
         driver.quit()
+        
+        soup = BeautifulSoup(target_source, "html.parser")
+        table = soup.find("table", attrs={"class":"table is-scrollable css-fssu8e is-fullwidth is-striped"}).find("tbody")
+        rows = table.find_all("tr")
+        for row in rows:
+            memory, vcpu, transfer, ssd, price_hour, price_month, register = row.find_all("td")
+            self.data.append({
+                "CPU / VCPU": vcpu.text, 
+                "MEMORY": memory.text, 
+                "STORAGE/SSD DISK": ssd.text, 
+                "BANDWIDTH / TRANSFER": transfer.text, 
+                "PRICE [ $/mo ]": price_month.text})
